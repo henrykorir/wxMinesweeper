0df81f5bf785e38dffac27f8ba81f2ff25a5e8f0
henrykorir <longkhery@gmail.com>
2024-05-07 18:52:02 +0300

    Added button click reveal

--- minesweeper.cpp
+++ minesweeper.cpp
@@ -10,43 +10,65 @@ class MainFrame : public wxFrame
 {
 public:
     MainFrame(const wxString&);
+    ~MainFrame();
     void OnExitProgram(wxCloseEvent&);
+    void OnButtonClick(wxCommandEvent&);
+
 private:
-    wxButton * matrix[81];
+    wxButton * matrix[9][9];
     wxGridSizer * gridSizer;
     wxDECLARE_EVENT_TABLE();
 };
 
 wxBEGIN_EVENT_TABLE(MainFrame, wxFrame)
     EVT_CLOSE(MainFrame::OnExitProgram)
+    EVT_BUTTON(wxID_ANY, MainFrame::OnButtonClick)
 wxEND_EVENT_TABLE()
 
 
 MainFrame::MainFrame(const wxString& title)
-    : wxFrame(NULL,wxID_ANY, title,wxDefaultPosition,wxDefaultSize,wxMINIMIZE_BOX | wxSYSTEM_MENU | wxCAPTION | wxCLOSE_BOX | wxCLIP_CHILDREN)
+    : wxFrame(NULL,wxID_ANY, title,wxDefaultPosition,wxDefaultSize,wxDEFAULT_DIALOG_STYLE | wxMINIMIZE_BOX)
 {
     gridSizer = new wxGridSizer(9,9,0,0);
 
-    for (int i = 0; i < 81; i++)
+    for (int i = 0; i < 9; i++)
     {
-        matrix[i] = new wxButton(this, wxID_ANY,wxEmptyString,wxDefaultPosition,wxSize(70,70));
-        gridSizer->Add(matrix[i]);
+        for (int j = 0; j < 9; j++)
+        {
+            matrix[i][j] = new wxButton(this, wxID_ANY,wxEmptyString,wxDefaultPosition,wxSize(70,70), wxBU_EXACTFIT);
+            gridSizer->Add(matrix[i][j]);
+        }
     }
 
     gridSizer->Fit(this);
     gridSizer->SetSizeHints(this);
 
+    SetSizer(gridSizer);
 
-    this->Centre();
+    Centre(wxBOTH);
+}
 
-    this->SetSizer(gridSizer);
+MainFrame::~MainFrame()
+{
+    for(int i = 0; i < 9; i++)
+        for(int j = 0; j < 9; j++)
+            delete matrix[i][j];
+    Destroy();
 }
 void MainFrame::OnExitProgram(wxCloseEvent& event)
 {
-    if (wxMessageBox("Are you sure?") == wxOK){
-        Destroy();
-    }
+    Destroy();
 }
+
+void MainFrame::OnButtonClick(wxCommandEvent& event)
+{
+    wxWindow * m_button = FindWindow(event.GetId());
+    m_button->SetFont(wxFont(13, wxFONTFAMILY_ROMAN, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD, false));
+    m_button->SetLabel("0");
+    m_button->Enable(false);
+    m_button->SetForegroundColour(wxColor(0,255,0));
+}
+
 wxDECLARE_APP(Minesweeper);
 wxIMPLEMENT_APP(Minesweeper);
 
