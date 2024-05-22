03d561210013efcf2b23653a2db3111bc20fefc5
henrykorir <longkhery@gmail.com>
2024-05-23 02:02:01 +0300

    added script to install wxwidgets in ubuntu

---
+++ script.sh
@@ -0,0 +1,9 @@
+sudo apt-get install -y build-essential libgtk2.0-dev libgtk-3-dev
+git clone https://github.com/wxWidgets/wxWidgets.git
+cd wxWidgets     
+mkdir gtk-build             # the name is not really relevant
+cd gtk-build
+../configure                # builds unicode, shared lib
+make -j3                    # use 3 cores. Set to the number of cores your have. 'make' uses 1 core
+sudo make install           # some platforms require to use 'su' instead of 'sudo'
+sudo ldconfig               # not required in each system
