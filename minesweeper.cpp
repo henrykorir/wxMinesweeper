fb875b8be34c9599b19a5190c4566f7a0b3efd9b
henrykorir <longkhery@gmail.com>
2024-06-05 15:53:37 +0300

    Added strategy for opening adjacent empty fields

--- minesweeper.cpp
+++ minesweeper.cpp
@@ -6,13 +6,12 @@
 
 wxBEGIN_EVENT_TABLE(MainFrame, wxFrame)
     EVT_CLOSE(MainFrame::OnExitProgram)
-wxEND_EVENT_TABLE()
+        wxEND_EVENT_TABLE()
 
-
-MainFrame::MainFrame(const wxString& title)
-    : wxFrame(NULL,wxID_ANY, title,wxDefaultPosition,wxDefaultSize,wxDEFAULT_DIALOG_STYLE | wxMINIMIZE_BOX)
+            MainFrame::MainFrame(const wxString &title)
+    : wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxDefaultSize, wxDEFAULT_DIALOG_STYLE | wxMINIMIZE_BOX)
 {
-    gridSizer = new wxGridSizer(9,9,0,0);
+    gridSizer = new wxGridSizer(9, 9, 0, 0);
 
     int id = 0;
 
@@ -20,8 +19,8 @@ MainFrame::MainFrame(const wxString& title)
     {
         for (int j = 0; j < 9; j++)
         {
-            matrix[i][j] =  new Field(this, i,j, FIELD_EMPTY,id);
-            if (rand()/(float)RAND_MAX < PROB)
+            matrix[i][j] = new Field(this, i, j, FIELD_EMPTY, id);
+            if (rand() / (float)RAND_MAX < PROB)
                 matrix[i][j]->SetType(FIELD_MINE);
             gridSizer->Add(matrix[i][j]);
             id++;
@@ -38,8 +37,8 @@ MainFrame::MainFrame(const wxString& title)
 
 MainFrame::~MainFrame()
 {
-    for(int i = 0; i < 9; i++)
-        for(int j = 0; j < 9; j++)
+    for (int i = 0; i < 9; i++)
+        for (int j = 0; j < 9; j++)
         {
             delete matrix[i][j]->GetButton();
             delete matrix[i][j];
@@ -47,7 +46,7 @@ MainFrame::~MainFrame()
     Destroy();
 }
 
-void MainFrame::OnExitProgram(wxCloseEvent& event)
+void MainFrame::OnExitProgram(wxCloseEvent &event)
 {
     Destroy();
 }
@@ -60,90 +59,82 @@ bool MainFrame::isFieldValid(int x, int y)
 void MainFrame::UnCover(int x, int y)
 {
     int countMines = 0;
+    wxStack<Field *> lookHaed;
 
-    if(isFieldValid(x-1, y))
+    if (isFieldValid(x - 1, y))
     {
-        if (matrix[x-1][y]->GetType() == FIELD_MINE) countMines++;
-        else if(!instack[matrix[x-1][y]->GetID()])
-        {
-            st.push(matrix[x-1][y]);
-            instack[matrix[x-1][y]->GetID()] = true;
-        }
-    }
-
-    if(isFieldValid(x-1, y+1))
-    {
-        if (matrix[x-1][y+1]->GetType() == FIELD_MINE) countMines++;
-        else if(!instack[matrix[x-1][y+1]->GetID()])
-        {
-            st.push(matrix[x-1][y+1]);
-            instack[matrix[x-1][y+1]->GetID()] = true;
-        }
-    }
-    if(isFieldValid(x, y+1))
-    {
-        if (matrix[x][y+1]->GetType() == FIELD_MINE) countMines++;
-        else if(!instack[matrix[x][y+1]->GetID()])
-        {
-            st.push(matrix[x][y+1]);
-            instack[matrix[x][y+1]->GetID()] = true;
-        }
-    }
-    if(isFieldValid(x+1, y+1))
-    {
-        if (matrix[x+1][y+1]->GetType() == FIELD_MINE) countMines++;
-        else if(!instack[matrix[x+1][y+1]->GetID()])
-        {
-            st.push(matrix[x+1][y+1]);
-            instack[matrix[x+1][y+1]->GetID()] = true;
-        }
-    }
-    if(isFieldValid(x+1, y))
-    {
-        if (matrix[x+1][y]->GetType() == FIELD_MINE) countMines++;
-        else if(!instack[matrix[x+1][y]->GetID()])
-        {
-            st.push(matrix[x+1][y]);
-            instack[matrix[x+1][y]->GetID()] = true;
-        }
-    }
-    if(isFieldValid(x+1, y-1))
-    {
-        if (matrix[x+1][y-1]->GetType() == FIELD_MINE) countMines++;
-        else if(!instack[matrix[x+1][y-1]->GetID()])
-        {
-            st.push(matrix[x+1][y-1]);
-            instack[matrix[x+1][y-1]->GetID()] = true;
-        }
-    }
-    if(isFieldValid(x, y-1))
-    {
-        if (matrix[x][y-1]->GetType() == FIELD_MINE) countMines++;
-        else if(!instack[matrix[x][y-1]->GetID()])
-        {
-            st.push(matrix[x][y-1]);
-            instack[matrix[x][y-1]->GetID()] = true;
-        }
-    }
-    if(isFieldValid(x-1, y-1))
-    {
-        if (matrix[x-1][y-1]->GetType() == FIELD_MINE) countMines++;
-        else if(!instack[matrix[x-1][y-1]->GetID()])
-        {
-            st.push(matrix[x-1][y-1]);
-            instack[matrix[x-1][y-1]->GetID()] = true;
-        }
+        if (matrix[x - 1][y]->GetType() == FIELD_MINE)
+            countMines++;
+        else if (!instack[matrix[x - 1][y]->GetID()])
+            lookHaed.push(matrix[x - 1][y]);
     }
 
-    matrix[x][y]->GetButton()->SetLabel(wxString::Format("%d",countMines));
+    if (isFieldValid(x - 1, y + 1))
+    {
+        if (matrix[x - 1][y + 1]->GetType() == FIELD_MINE)
+            countMines++;
+        else if (!instack[matrix[x - 1][y + 1]->GetID()])
+            lookHaed.push(matrix[x - 1][y + 1]);
+    }
+    if (isFieldValid(x, y + 1))
+    {
+        if (matrix[x][y + 1]->GetType() == FIELD_MINE)
+            countMines++;
+        else if (!instack[matrix[x][y + 1]->GetID()])
+            lookHaed.push(matrix[x][y + 1]);
+    }
+    if (isFieldValid(x + 1, y + 1))
+    {
+        if (matrix[x + 1][y + 1]->GetType() == FIELD_MINE)
+            countMines++;
+        else if (!instack[matrix[x + 1][y + 1]->GetID()])
+            lookHaed.push(matrix[x + 1][y + 1]);
+    }
+    if (isFieldValid(x + 1, y))
+    {
+        if (matrix[x + 1][y]->GetType() == FIELD_MINE)
+            countMines++;
+        else if (!instack[matrix[x + 1][y]->GetID()])
+            lookHaed.push(matrix[x + 1][y]);
+    }
+    if (isFieldValid(x + 1, y - 1))
+    {
+        if (matrix[x + 1][y - 1]->GetType() == FIELD_MINE)
+            countMines++;
+        else if (!instack[matrix[x + 1][y - 1]->GetID()])
+            lookHaed.push(matrix[x + 1][y - 1]);
+    }
+    if (isFieldValid(x, y - 1))
+    {
+        if (matrix[x][y - 1]->GetType() == FIELD_MINE)
+            countMines++;
+        else if (!instack[matrix[x][y - 1]->GetID()])
+            lookHaed.push(matrix[x][y - 1]);
+    }
+    if (isFieldValid(x - 1, y - 1))
+    {
+        if (matrix[x - 1][y - 1]->GetType() == FIELD_MINE)
+            countMines++;
+        else if (!instack[matrix[x - 1][y - 1]->GetID()])
+            lookHaed.push(matrix[x - 1][y - 1]);
+    }
+
+    matrix[x][y]->GetButton()->SetLabel(wxString::Format("%d", countMines));
     visited[matrix[x][y]->GetID()] = true;
 
-    if(st.empty()) return;
-    Field * top = st.top();
+    if (st.empty() || countMines > 0)
+        return;
+    while (lookHaed.size() > 0)
+    {
+        Field *field = lookHaed.top();
+        instack[field->GetID()] = true;
+        st.push(field);
+        lookHaed.pop();
+    }
+    Field *top = st.top();
     st.pop();
 
     UnCover(top->GetX(), top->GetY());
-
 }
 
 wxDECLARE_APP(Minesweeper);
@@ -151,12 +142,12 @@ wxIMPLEMENT_APP(Minesweeper);
 
 bool Minesweeper::OnInit()
 {
-    srand((unsigned) time(NULL));
+    srand((unsigned)time(NULL));
 
     if (!wxApp::OnInit())
         return false;
 
-    MainFrame * frame = new MainFrame(_("Minesweeper"));
+    MainFrame *frame = new MainFrame(_("Minesweeper"));
     frame->Show();
 
     return true;
